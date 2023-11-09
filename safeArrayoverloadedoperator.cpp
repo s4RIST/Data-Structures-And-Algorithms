9242be0d00d18baa934c2f025023c1438f79207a
s4RIST <kamiljadachy@gmail.com>
2023-11-09 15:01:11 +0000

    Create safeArrayoverloadedoperator.cpp

---
+++ safeArrayoverloadedoperator.cpp
@@ -0,0 +1,12 @@
+safeArray& safeArray::operator=(safeArray& rhs) 
+{
+    if (this != &rhs)
+        {
+        for (int i = 0; i < size; i++) {
+            }
+        }
+        cout << "In the Copy Assignment Operator" << endl;
+        
+
+    return rhs;
+}
